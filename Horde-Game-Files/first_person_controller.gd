f87fd22d4aad19089f0bf1a35b3f89dc1df128aa
STVND <42823678+STVND@users.noreply.github.com>
2023-02-04 16:53:17 -0800

    Mouse support and menu refinement
    Improved menu as well as added mouse support for looking around

--- Horde-Game-Files/first_person_controller.gd
+++ Horde-Game-Files/first_person_controller.gd
@@ -2,23 +2,36 @@ extends CharacterBody3D
 
 
 @export var speed = 5
-@export var fall_acceleration = 50
+@export var fall_acceleration = 75
 @export var jump_impulse = 15
+@export var mouse_sensitivity = .01
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _input(event):
+	if event is InputEventMouseMotion  and $game_session_menu.visible == false:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -PI/2, PI/2)
+
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		$game_session_menu.visible = true
+		
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	var direction = Vector3.ZERO
-
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	if $game_session_menu.visible == true:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
